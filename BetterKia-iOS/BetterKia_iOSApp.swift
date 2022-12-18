4b8dc1823a6ac5368a8fa215b292e1be19181478
Joschua H <joschua.hass.sh@gmail.com>
2022-12-18 18:01:17 +0100

    Initial Commit

---
+++ BetterKia-iOS/BetterKia_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterKia_iOSApp.swift
+//  BetterKia-iOS
+//
+//  Created by Joschua Haß on 18.12.22.
+//
+
+import SwiftUI
+
+@main
+struct BetterKia_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
