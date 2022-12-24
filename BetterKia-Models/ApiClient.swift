a934a7ecd113320a7a7c40a0d969647538d59f10
Joschua H <joschua.hass.sh@gmail.com>
2022-12-24 13:07:32 +0100

    .

---
+++ BetterKia-Models/ApiClient.swift
@@ -0,0 +1,17 @@
+//
+//  ApiClient.swift
+//  BetterKia-iOS
+//
+//  Created by Joschua Haß on 23.12.22.
+//
+
+import Foundation
+
+public class ApiClient {
+    private let _authHeader = Data("2384z27834687236478f67826482|fjfiuwergisidjb4r734fsj3".utf8).base64EncodedString()
+    
+    static func registerDeviceToken(deviceToken: String) {
+        
+    }
+    
+}
--- BetterKia-iOS/AppDelegate.swift
+++ BetterKia-iOS/AppDelegate.swift
@@ -11,10 +11,25 @@ import os
 import WidgetKit
 
 class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
-    func application(
-        _ application: UIApplication,
-        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-    ) {
-        // Record the device token.
+    func application(_ application: UIApplication,
+               didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+       // Override point for customization after application launch.you’re
+       UIApplication.shared.registerForRemoteNotifications()
+       return true
+    }
+
+    func application(_ application: UIApplication,
+                didRegisterForRemoteNotificationsWithDeviceToken
+                    deviceToken: Data) {
+       // ToDo: send deviceToken to server
+        let token = deviceToken.map { String(format: "%.2hhx", $0) }.joined()
+        Logger().log("Device token: \(token)")
+    }
+
+    func application(_ application: UIApplication,
+                didFailToRegisterForRemoteNotificationsWithError
+                    error: Error) {
+       // Try again later.
     }
 }
